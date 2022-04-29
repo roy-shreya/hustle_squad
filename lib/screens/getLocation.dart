91a5211c5bbb07a29d39be50455f9c8ee6079b9b
Sangeetha N M <sangeethasn2000@gmail.com>
2022-04-30 01:57:04 +0530

    SOS done

--- lib/screens/getLocation.dart
+++ lib/screens/getLocation.dart
@@ -12,6 +12,7 @@ import 'package:telephony/telephony.dart';
 import 'package:geolocator/geolocator.dart' as geolocator;
 import 'dart:math';
 import 'package:geoflutterfire/geoflutterfire.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class GetLocation extends StatefulWidget {
   const GetLocation({Key? key}) : super(key: key);
@@ -64,14 +65,14 @@ class _GetLocationState extends State<GetLocation> {
     super.dispose();
   }
 
-  Timer _timer = Timer.periodic(Duration(seconds: 1), (timer) {});
+  Timer _timer = Timer.periodic(const Duration(seconds: 1), (timer) {});
   void _startTimer() {
     _counter = 10;
     // ignore: unnecessary_null_comparison
     if (_timer != null) {
       _timer.cancel();
     }
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       if (_counter > 0) {
         _counter--;
       } else {
@@ -85,7 +86,7 @@ class _GetLocationState extends State<GetLocation> {
 
   void alertD(BuildContext ctx) {
     var alert = AlertDialog(
-        shape: RoundedRectangleBorder(
+        shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(20.0))),
         backgroundColor: Colors.grey[100],
         elevation: 0.0,
@@ -98,8 +99,8 @@ class _GetLocationState extends State<GetLocation> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    Padding(
-                        padding: const EdgeInsets.only(
+                    const Padding(
+                        padding: EdgeInsets.only(
                             top: 10, left: 10, right: 10, bottom: 15),
                         child: Text(
                           'Are you sure you want to send SOS?',
@@ -108,14 +109,14 @@ class _GetLocationState extends State<GetLocation> {
                               fontWeight: FontWeight.bold,
                               fontSize: 20),
                         )),
-                    SizedBox(
+                    const SizedBox(
                       height: 5,
                     ),
                     Text(
                       '${snapshot.data.toString()}',
-                      style: TextStyle(fontSize: 25),
+                      style: const TextStyle(fontSize: 25),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 30,
                     ),
                     Row(
@@ -138,15 +139,15 @@ class _GetLocationState extends State<GetLocation> {
                                 height: 50,
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(25),
-                                  gradient: LinearGradient(
+                                  gradient: const LinearGradient(
                                       colors: [
-                                        Color(0xff09009B),
-                                        Color(0xff193498),
+                                        const Color(0xff09009B),
+                                        const Color(0xff193498),
                                       ],
                                       begin: Alignment.topLeft,
                                       end: Alignment.bottomRight),
                                 ),
-                                child: Center(
+                                child: const Center(
                                     child: Text(
                                   'No',
                                   style: TextStyle(
@@ -293,72 +294,116 @@ if(this.mounted){
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xff21254A),
-        body: Container(
-          child: Column(
-            children: <Widget>[
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color(0xffFAF9F6),
+                Color(0xffE3CBF4),
+              ]),
+        ),
+        child: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 110,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 16, top: 0.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Text(
+                        "Save Our Ship(SOS)",
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontFamily: 'Sans-Pro',
+                            letterSpacing: 0.3,
+                            overflow: TextOverflow.fade,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Are you in a trouble?",
+                        style: TextStyle(
+                            fontFamily: 'Sans-Pro',
+                            color: Color(0xffa29aac),
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        "Don't worry we are here to help you!",
+                        style: TextStyle(
+                            fontFamily: 'Sans-Pro',
+                            //letterSpacing: 0.5,
+                            color: Color(0xffa29aac),
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+               
+                ],
+                
+              ),
+            ), 
+            SizedBox(
+              height: 100,
+            ),
+              new Container(
+                    child: new Image.asset(
+                          'images/sos.png',
+                          height: 300.0,
+                          fit: BoxFit.fitWidth,
+                        )
+                 ),  
+            const SizedBox(
+              height: 50,
+            ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                              "Your current location",
+                              style: TextStyle(
+                                  fontFamily: 'Sans-Pro',
+                                  //letterSpacing: 0.5,
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                    ],
+                  ),
+                ),
+                     const SizedBox(
+              height: 10,
+            ),
               Padding(
-                padding: EdgeInsets.only(left: 15.0, right: 8.0, top: 50.0),
+                padding: EdgeInsets.only(left: 20.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(
-                        Icons.arrow_back,
-                        size: 24.0,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => WelcomePage()));
-                      },
-                    ),
-                    Text(
-                      'Getting location',
-                      style: TextStyle(
-                          fontFamily: 'Sans-Pro',
-                          fontWeight: FontWeight.w700,
-                          color: Colors.white,
-                          fontSize: 23,
-                          letterSpacing: 0.6),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.only(top: 150.0, left: 20.0, right: 20.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Icon(
-                      Icons.location_on,
-                      size: 45,
-                      color: Colors.blue,
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Text(
-                      "Current Location: ",
-                      style: TextStyle(
-                        fontSize: 26.0,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      locationMessage,
-                      style: TextStyle(fontSize: 15.0, color: Colors.white),
-                    ),
+                    GestureDetector(
+                child: Text("Link", style: TextStyle(decoration: TextDecoration.underline, color: Colors.blue)),
+                onTap: () async {
+                  if (await canLaunch(locationMessage)) launch(locationMessage);
+                },
+              ),
                   ],
                 ),
-              ),
-            ],
-          ),
-        ));
+              ) 
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/search_contacts.dart
@@ -0,0 +1,144 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:contacts_service/contacts_service.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:cloud_firestore/cloud_firestore.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SearchContacts extends StatefulWidget {
+  SearchContacts({Key? key}) : super(key: key);
+
+  @override
+  _SearchContactsState createState() => _SearchContactsState();
+}
+
+class _SearchContactsState extends State<SearchContacts> {
+  List<Contact> contacts = [];
+  List<Contact> contactsFiltered = [];
+  List<Contact> emergContacts = [];
+  final user = FirebaseAuth.instance.currentUser;
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  TextEditingController serachController = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+     getAllContacts();
+   serachController.addListener(() {
+      filterContacts();
+     });
+     
+  }
+
+  Future<void> getAllContacts() async {
+    List<Contact> _contacts =
+        (await ContactsService.getContacts(withThumbnails: false)).toList();       
+    setState(() {
+      contacts = _contacts;
+    });
+  }
+
+ Future<void> filterContacts() async {
+    List<Contact> _contacts = [];
+    
+    _contacts.addAll(contacts);
+    if (serachController.text.isNotEmpty) {
+      _contacts.retainWhere((contact) {
+        String serachTerm = serachController.text.toLowerCase();
+        var n;
+        n = null;
+        var phone = contact.phones?.firstWhere((phn) {
+          return phn.value!.contains(serachTerm);
+        }, orElse: () => n);
+        return phone != null;
+      });
+      setState(() {
+        contactsFiltered = _contacts;
+      });
+    }
+   
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isSearching = serachController.text.isNotEmpty;
+
+    Future<void> getDoc(String name, var phn) async  {
+      var a =  await users
+          .doc(user!.email)
+          .collection('emergencyContacts')
+          .doc(name)
+          .get();
+      if (!a.exists) {
+        users
+            .doc(user!.email)
+            .collection('emergencyContacts')
+            .doc(name)
+            .set({'name': name, 'number': phn});
+      } else {
+        print("Contacts Exists");
+      }
+    }
+
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Column(children: <Widget>[
+        Container(
+          child: TextField(
+            cursorColor: Colors.black,
+            controller: serachController,
+            decoration: InputDecoration(
+                labelText: "Search",
+                border: OutlineInputBorder(
+                    borderSide: new BorderSide(color: Colors.white)),
+                prefixIcon: Icon(Icons.search, color: Colors.blueGrey)),
+          ),
+        ),
+       Expanded( 
+          child:  ListView.builder(
+              shrinkWrap: true,
+              itemCount: isSearching == true ? contactsFiltered.length : 0,
+              itemBuilder: (context, index) {
+                Contact contact =  contactsFiltered[index];
+               return ListTile(
+                    onTap: () {
+                      setState(() {
+                        emergContacts.add(contact);
+                        var phn = contact.phones!.elementAt(0).value;
+                        phn = phn!.replaceAll(' ', '');
+                        if (phn.substring(0, 3) == "+91")
+                          phn = phn.substring(3);
+                        getDoc(contact.displayName.toString(), phn);
+                      });
+                    },
+                    title: Text(
+                      contact.displayName ?? "",
+                      style: TextStyle(
+                          fontFamily: 'Sans-Pro', color: Colors.black),
+                    ),
+                    subtitle: Text(
+                      contact.phones!.elementAt(0).value ?? '',
+                      style: TextStyle(
+                          fontFamily: 'Sans-Pro', color: Colors.black),
+                    ));
+              })
+       ),
+        Padding(
+          padding: const EdgeInsets.only(left: 200.0, bottom: 10.0),
+          child: FloatingActionButton(
+              mini: true,
+              child: Icon(Icons.done_rounded),
+              backgroundColor: Color(0xff5089C6),
+              onPressed: () {
+                Navigator.of(context, rootNavigator: true).pop('dialog');
+              }),
+        )
+      ]),
+    );
+  }
+}
