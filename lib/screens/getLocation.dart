f84a71db5bb2e618d984b659dae4812b88f41857
janhavi kulkarni <janhavi1dkulkarni@gmail.com>
2022-04-30 09:18:41 +0530

    Deleting files...

--- lib/screens/getLocation.dart
+++ lib/screens/getLocation.dart
@@ -6,6 +6,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:nmithacks/screens/homepage_widget.dart';
+import 'package:nmithacks/screens/notification.dart';
 import 'package:nmithacks/welcome.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:telephony/telephony.dart';
@@ -270,6 +271,8 @@ if(this.mounted){
     getPoliceStationDetails();
   }
 
+static final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
   Future getPoliceStationDetails() async {
     DocumentReference sub = _firestore
         .collection('policeStation/Bengaluru/local/')
@@ -280,6 +283,8 @@ if(this.mounted){
     print('The list is');
     print(emergencyRadiusContacts);
     _sendSMS("I NEED HELP! Mylocation: $locationMessage");
+     final pushNotificationService = PushNotificationService(_firebaseMessaging);
+    pushNotificationService.initialise();
   }
 
   Future<void> _sendSMS(msg) async {
@@ -292,30 +297,9 @@ if(this.mounted){
     }
   }
 
-  late final FirebaseMessaging _messaging;
+  
 
-  void registerNotification() async {
-  // 1. Initialize the Firebase app
-  await Firebase.initializeApp();
 
-  // 2. Instantiate Firebase Messaging
-  _messaging = FirebaseMessaging.instance;
-
-  // 3. On iOS, this helps to take the user permissions
-  NotificationSettings settings = await _messaging.requestPermission(
-    alert: true,
-    badge: true,
-    provisional: false,
-    sound: true,
-  );
-
-  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-    print('User granted permission');
-    // TODO: handle the received notifications
-  } else {
-    print('User declined or has not accepted permission');
-  }
-}
 
   @override
   Widget build(BuildContext context) {
