2866916d33d73f3481267ee74d471c9c58ac3691
janhavi kulkarni <janhavi1dkulkarni@gmail.com>
2022-04-30 07:31:29 +0530

    Emergency contacts...

--- lib/screens/getLocation.dart
+++ lib/screens/getLocation.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:nmithacks/screens/homepage_widget.dart';
@@ -290,6 +291,31 @@ if(this.mounted){
       );
     }
   }
+
+  late final FirebaseMessaging _messaging;
+
+  void registerNotification() async {
+  // 1. Initialize the Firebase app
+  await Firebase.initializeApp();
+
+  // 2. Instantiate Firebase Messaging
+  _messaging = FirebaseMessaging.instance;
+
+  // 3. On iOS, this helps to take the user permissions
+  NotificationSettings settings = await _messaging.requestPermission(
+    alert: true,
+    badge: true,
+    provisional: false,
+    sound: true,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print('User granted permission');
+    // TODO: handle the received notifications
+  } else {
+    print('User declined or has not accepted permission');
+  }
+}
 
   @override
   Widget build(BuildContext context) {
